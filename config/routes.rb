24f1643e2f075051cef44c54eeefa8dcf0338309
hayaki <hayaki@hayakis-MacBook-Pro.local>
2017-03-07 17:39:15 +0900

    [modify] name devise to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
-  get 'home/index'
+  get    '/home',        to: 'home#index'
+
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+    email: 'info@example.com', password: 'admins', password_confirmation: 'admins'
+)
